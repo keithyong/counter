5a75964692f1b8463e668b3be9f2dff03a18c6a7
Keith Yong <terda12@gmail.com>
2015-01-03 10:03:14 -0500

    Initial Commit

---
+++ KeithRPG/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  KeithRPG
+//
+//  Created by Keith Yong on 1/3/15.
+//  Copyright (c) 2015 keith. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
